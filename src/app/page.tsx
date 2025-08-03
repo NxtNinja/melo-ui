4afb0c379726ed5d03cd644301272fa2ac5373bf
PRIYANGSU BANIK <136341993+NxtNinja@users.noreply.github.com>
2025-08-03 19:27:39 +0530

    middleware removed, proxy added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import ProtectedLayout from "@/components/layouts/ProtectedLayout";
 import LikeButton from "@/components/shsfui/button/like-button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <LikeButton />
-    </div>
+    <ProtectedLayout>
+      <div className="flex justify-center items-center h-screen">
+        <LikeButton />
+      </div>
+    </ProtectedLayout>
   );
 }
